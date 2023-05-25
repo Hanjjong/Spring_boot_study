19bae9a3fbdbdc1ce7d9cab857e657de794c375c
Hanjjong <tim4974@naver.com>
2023-05-25 20:24:03 +0900

    Create MemberRepository, MemberService

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,10 +1,14 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
@@ -12,9 +16,12 @@ public class Member {
     private Long id;
 
     private String name;
+
     @Embedded
     private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
+
 }
