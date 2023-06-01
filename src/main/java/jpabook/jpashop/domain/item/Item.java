30f47dc18417cde0e0dc6d804ef069bf6621d7b2
Hanjjong <tim4974@naver.com>
2023-06-01 17:17:03 +0900

    create controller

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.Exception.NotEnoughStockException;
 import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,5 +26,19 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> child = new ArrayList<>();
+
+
+    // ==비즈니스 로직==//
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
